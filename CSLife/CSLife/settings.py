d7115ac555e52a04e1e18a2c37aa99adcf1bf842
AlexLex13 <93290744+AlexLex13@users.noreply.github.com>
2022-07-17 15:17:40 +0300

    No secrets

--- CSLife/CSLife/settings.py
+++ CSLife/CSLife/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5t^k)^^&ri9h9@ajn&%*5ll4wva%jt3+88lup7x+f=6=tfcs-d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
